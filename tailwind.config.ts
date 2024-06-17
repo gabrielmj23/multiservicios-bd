604dc2c32c5a2678f867b0d333f625e91714d94d
gabrielmj23 <gabrielmj2312@gmail.com>
2024-06-17 17:46:14 -0400

    Login y plantilla de dashboard

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import * as flowbite from "flowbite-react/tailwind";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", flowbite.content()],
   theme: {
+    fontSize: {
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "3.75rem",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 } satisfies Config;
-
